002785a9ed835fa4179cc2e33302d81446ba0d7d
Jose Luis Sanchez <jlleluis@gmail.com>
2020-07-04 00:52:15 -0500

    funcionalidad tabla

---
+++ application/views/escuela/tabla_nlogro.php
@@ -0,0 +1,112 @@
+<div class="table-responsive">
+<table id="tabla_planea" class="table table-gray table-hover">
+	<thead>
+		<tr>
+			<th class="text-center" rowspan="2"></th>
+			<th class="text-center" colspan="4">Lenguaje y Comunicación</th>
+			<th class="text-center" colspan="4">Matemáticas</th>
+		</tr>
+		<tr>
+			<th class="text-center">I
+				<br><span style="font-weight:normal">Insuficiente</span>
+			</th>
+			<th class="text-center">II
+				<br><span style="font-weight:normal">Elemental</span>
+			</th>
+			<th class="text-center">III
+				<br><span style="font-weight:normal">Bueno</span>
+			</th>
+			<th class="text-center">IV
+				<br><span style="font-weight:normal">Excelente</span>
+			</th>
+			<th class="text-center">I
+				<br><span style="font-weight:normal">Insuficiente</span>
+			</th>
+			<th class="text-center">II
+				<br><span style="font-weight:normal">Elemental</span>
+			</th>
+			<th class="text-center">III
+				<br><span style="font-weight:normal">Bueno</span>
+			</th>
+			<th class="text-center">IV
+				<br><span style="font-weight:normal">Excelente</span>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td colspan="9" style="background-color:silver;">PLANEA 2016</td>
+		</tr>
+		<tr>
+			<th class="text-center">Tu escuela</th>
+			<th class="text-center">11.9%</th>
+			<th class="text-center">30.5%</th>
+			<th class="text-center">28.8%</th>
+			<th class="text-center">28.8%</th>
+			<th class="text-center">32.2%</th>
+			<th class="text-center">15.3%</th>
+			<th class="text-center">18.6%</th>
+			<th class="text-center">33.9%</th>
+		</tr>
+		<tr>
+			<th class="text-center">Estado de Coahuila</th>
+			<th class="text-center">36.65%</th>
+			<th class="text-center">34.72%</th>
+			<th class="text-center">21.97%</th>
+			<th class="text-center">6.66%</th>
+			<th class="text-center">47.67%</th>
+			<th class="text-center">18.51%</th>
+			<th class="text-center">19.01%</th>
+			<th class="text-center">14.8%</th>
+		</tr>
+		<tr>
+			<th class="text-center">Nacional</th>
+			<th class="text-center">36.5%</th>
+			<th class="text-center">34.6%</th>
+			<th class="text-center">21.8%</th>
+			<th class="text-center">7.1%</th>
+			<th class="text-center">47.8%</th>
+			<th class="text-center">18.5%</th>
+			<th class="text-center">18.7%</th>
+			<th class="text-center">15%</th>
+		</tr>
+
+		<tr>
+			<td colspan="9" style="background-color:silver;">PLANEA 2018</td>
+		</tr>
+		<tr>
+			<th class="text-center">Tu escuela</th>
+			<th class="text-center">11.9%</th>
+			<th class="text-center">30.5%</th>
+			<th class="text-center">28.8%</th>
+			<th class="text-center">28.8%</th>
+			<th class="text-center">32.2%</th>
+			<th class="text-center">15.3%</th>
+			<th class="text-center">18.6%</th>
+			<th class="text-center">33.9%</th>
+		</tr>
+		<tr>
+			<th class="text-center">Estado de Coahuila</th>
+			<th class="text-center">36.65%</th>
+			<th class="text-center">34.72%</th>
+			<th class="text-center">21.97%</th>
+			<th class="text-center">6.66%</th>
+			<th class="text-center">47.67%</th>
+			<th class="text-center">18.51%</th>
+			<th class="text-center">19.01%</th>
+			<th class="text-center">14.8%</th>
+		</tr>
+		<tr>
+			<th class="text-center">Nacional</th>
+			<th class="text-center">36.5%</th>
+			<th class="text-center">34.6%</th>
+			<th class="text-center">21.8%</th>
+			<th class="text-center">7.1%</th>
+			<th class="text-center">47.8%</th>
+			<th class="text-center">18.5%</th>
+			<th class="text-center">18.7%</th>
+			<th class="text-center">15%</th>
+		</tr>
+	</tbody>
+</table>
+</div>
